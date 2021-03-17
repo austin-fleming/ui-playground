ebb923d97d2a259db04d20d5f408eb52cf3e82ab
Austin <austin@flimflamfactory.com>
2021-03-17 01:12:27 -0500

    Update fp.js

--- vector-calculator-02/js/module/fp.js
+++ vector-calculator-02/js/module/fp.js
@@ -3,3 +3,23 @@ const pipeAll = (...functionList) => (input) =>
 		(accumulator, currentFunction) => currentFunction(accumulator),
 		input
 	);
+
+const stringToNumber = (stringValue) => +stringValue;
+
+const cloneArray = (inputArray) => [...inputArray];
+
+const returnTrueIfNull = (input) => input ?? true;
+
+const returnTrueIfNullOrEmpty = (input) => input || true;
+
+const exampleObject = Object.freeze({ a: 0, b: 1, c: 2 });
+
+const { a, b, c } = exampleObject;
+
+const callFunctionIfTrue = (maybeTrue, funcToCall) => maybeTrue && funcToCall();
+
+const callFunctionIfFalse = (maybeTrue, funcToCall) =>
+	maybeTrue || funcToCall();
+
+const returnTrueIfArrayContainsItem = (inputArray, item) =>
+	~inputArray.indexOf(item);
